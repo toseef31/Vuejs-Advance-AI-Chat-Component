525b3abe601853b739318822ce75c75b211ad0f7
toseef31 <peekinternational@gmail.com>
2025-05-23 19:10:43 +0500

    Removed header

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld icon="pi pi-thumbs-up" severity="success" />
-    </div>
-  </header>
+  <HelloWorld icon="pi pi-thumbs-up" severity="success" />
 </template>
 
 <style scoped>
